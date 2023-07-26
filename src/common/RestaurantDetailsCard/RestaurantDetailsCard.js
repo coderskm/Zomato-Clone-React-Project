e58ac47073b3c20cbedcd6ea61d2e66c559f0365
Sumit Kumar Mishra <sumitkumarmishra.code@gmail.com>
2023-07-26 23:15:07 +0530

    app

--- src/common/RestaurantDetailsCard/RestaurantDetailsCard.js
+++ src/common/RestaurantDetailsCard/RestaurantDetailsCard.js
@@ -4,20 +4,24 @@ function RestaurantDetail({ image, coupon, title, rating, description, price, ti
   return (
     <div className="restaurant-details-card-container">
       <div>
-        <img src="" alt="" className="restaurant-details-card-image" />
+        <img
+          src="https://b.zmtcdn.com/data/pictures/chains/8/19803678/365eb84dced494844fc142944ec7272b_o2_featured_v2.jpg"
+          alt="restaurant"
+          className="restaurant-details-card-image"
+        />
       </div>
       <div className="restaurant-details-card-title-container">
-        <Text text="The Cke Comapny" variant="h4" />
+        <Text text="The Cake Comapny" variant="h4" />
         <div className="restaurant-details-card-rating">
-                  <Text text="4" variant="bold-p" />
-                  <p>&#9734;</p>
+          <Text text="4" variant="bold-p" />
+          <p>&#9734;</p>
         </div>
       </div>
       <div className="restaurant-details-card-description-container">
         <div className="restaurant-details-card-description">
-          <Text text="" />
+          <Text text="Ice Cream, Mithai, Desserts" />
         </div>
-        <Text text="" />
+        <Text text="₹100" />
       </div>
       <div className="restaurant-details-card-time">
         <Text text="34 min" variant="p2" />
